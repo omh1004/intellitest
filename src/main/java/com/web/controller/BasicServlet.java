bfb2209a20d6126d6979c137226fe05be397045a
omh1004 <ohchef2020@gmail.com>
2025-01-14 10:49:35 +0900

    commit init

---
+++ src/main/java/com/web/controller/BasicServlet.java
@@ -0,0 +1,22 @@
+package com.web.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class BasicServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("인텔리제이 좋네!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; UTF-8" pageEncoding="UTF-8"%>
+
+<html>
+<body>
+    <h2>나의 첫번째 인텔리제이!</h2>
+    <h3>
+        <a href="test">나의 첫 서블릿</a>
+    </h3>
+
+</body>
+</html>
